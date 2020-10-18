313185573a868ce8e6d5b351a296465cac01c816
alirezava4idi <alireza.vahidi1@gmail.com>
2020-10-18 18:48:16 +0330

    added database connection and added inser new user.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const db = require("./db")
+const bcrypt = require("bcrypt")
 const { json } = require("express")
 const express = require("express")
 const app = express()
@@ -6,7 +8,7 @@ const PORT = 3000
 
 app.use(json());
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
     const user = req.body
     if("email" in user && "password" in user && Object.keys(user).length == 2){
         if (!validator.isEmail(user.email) && user.email.trim() != "") {
@@ -15,9 +17,22 @@ app.post("/signup", (req, res) => {
             res.send({"error": true, "message": "Invalid Password"})
         }else{
             // put in database
+            const saltRounds = 8;
             const email = user.email.trim()
             const password = user.password.trim()
-            res.send({email, password})
+            let hash = await bcrypt.hash(password, saltRounds);
+            db.connection.connect((err) => {
+                if (err) throw err;
+                console.log("Connected!");
+                var sql =
+                        `INSERT INTO users (email, password) VALUES ('${email}', '${hash}')`;
+                db.connection.query(sql, (err, result) => {
+                    if (err) throw err;
+                    console.log("1 record inserted");
+                    res.send({"error": false, "message": "Your are a member now!"});
+                });
+            });
+            
         }
     }else{
         res.send({"error": true, "message": "Invalid form request"});
